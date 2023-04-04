e5644ec06c922458f268faec87757900e7155a79
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-04-04 11:30:35 +0700

    Add interval, epsilon & SEIRD constantsf

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    double a = 0, b = 90, eps = 1e-2;
+    double alphaE = 0.999, alphaI = 0.999, k = 0.042, ro = 0.952, beta = 0.999, mu = 0.0188, cIsol = 0, E0 = 99, R0 = 24;
+
+    return 0;
+}
