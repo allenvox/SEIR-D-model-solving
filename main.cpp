587e268d60fd9f27e9e216a77ae43d22b6ac54e1
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-04-10 16:52:13 +0700

    Implement differential equations' functions

--- main.cpp
+++ main.cpp
@@ -11,6 +11,34 @@
 #define R0 24         // начальное количество вылеченных
 #define GAMMA 0       // скорость повторного заражения, раз (0 - устойчивый иммунитет)
 #define TETTA 2       // латетный/инкубационный период (запаздывание заразности), дни
+#define C             //
+
+double dS_dt(double S, double E, double I, double R, double D)
+{
+    double N = S + E + I + R + D;
+    return -C * (ALPHA_I * S * I + ALPHA_E * S * E) / N + GAMMA * R;
+}
+
+double dE_dt(double S, double E, double I, double R, double D)
+{
+    double N = S + E + I + R + D;
+    return C * (ALPHA_I * S * I + ALPHA_E * S * E) / N - (K + RO) * E;
+}
+
+double dI_dt(double S, double E, double I, double R, double D)
+{
+    return K * E - BETA * I - MU * I;
+}
+
+double dR_dt(double S, double E, double I, double R, double D)
+{
+    return BETA * I + RO * E - GAMMA * R;
+}
+
+double dD_dt(double S, double E, double I, double R, double D)
+{
+    return MU * I;
+}
 
 int main()
 {
