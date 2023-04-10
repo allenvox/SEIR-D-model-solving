5326d8d3e807529c0c6fe364af46657b1dc4f5c7
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-04-10 16:21:48 +0700

    Move constants to defines

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+#define MU 0.0188
+#define BETA 0.999
+#define RO 0.952
+#define ALPHA_E 0.999
+#define ALPHA_I 0.999
+#define K 0.042
+#define C_ISOL 0
+#define E0 99
+#define R0 24
+
 int main()
 {
     double a = 0, b = 90, eps = 1e-2;
-    double alphaE = 0.999, alphaI = 0.999, k = 0.042, ro = 0.952, beta = 0.999, mu = 0.0188, cIsol = 0, E0 = 99, R0 = 24;
+
+    std::cout.precision(5);
+    std::cout.setf(std::ios::fixed);
 
     return 0;
 }
